ca862d4acbf932e15acbe953ae14ad0236ccb692
Xenoxo <timwyc@gmail.com>
2025-08-15 20:49:30 +0200

    Added character flipping and enemy assets

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,10 +9,14 @@ extends CharacterBody2D
 @onready var hitbox_collider: CollisionShape2D = $HitboxComponent/HitboxCollider
 @onready var hitbox_component: HitboxComponent = $HitboxComponent
 
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
+@onready var hurtbox_collider: CollisionShape2D = $HurtboxComponent/HurtboxCollider
+
 @export var speed: float = 100.0
 @export var ATTACK_DAMAGE = 5
 
 var screen_size: Vector2
+var facing_left: bool = false
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
@@ -28,16 +32,34 @@ func _ready() -> void:
 func _physics_process(delta) -> void:
 	handle_player_input()
 
+func set_facing_direction(input_direction: Vector2):
+
+	if input_direction.x < 0.0:
+		facing_left = true
+
+		#print(hurtbox_component.position.x)
+	elif input_direction.x > 0.0:
+		facing_left = false
+		animated_sprite_2d.flip_h = facing_left
+		#hurtbox_component.position.x *= -1
+	animated_sprite_2d.flip_h = facing_left
 
 func handle_player_input():
 	var input_direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-
+	set_facing_direction(input_direction)
 	if !is_animation_still_playing("attack"):
 		if Input.is_action_pressed("action"):
+	#		animated_sprite_2d.flip_h = facing_direction
+			if facing_left:
+				hitbox_collider.position.x = -45.0
+				print(hitbox_collider.position.x)
+			else:
+				hitbox_collider.position.x = 45.0
 			state_machine.change_to("Attack")
 		else:
 			velocity = input_direction * speed
 			if velocity != Vector2.ZERO:
+				#print(input_direction.x)
 				state_machine.change_to("Run")
 			else:
 				state_machine.change_to("Idle")
